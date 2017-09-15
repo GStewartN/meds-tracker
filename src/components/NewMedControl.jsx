0c550b577806999038ced4af66e83970f179ff9e
Nathan Stewart <stewart.nathang@gmail.com>
2017-09-15 12:42:01 -0700

    make newmedcontrol comp stateful, add button event handler

--- src/components/NewMedControl.jsx
+++ src/components/NewMedControl.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-function NewMedControl(){
-  return(
-    <div>
-      <h5>Click the button to begin tracking a medication.</h5>
-      <Button>Add Medication</Button>
-      <hr/>
-    </div>
-  );
+class NewMedControl extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {formVisibleOnPage: false};
+    this.handleDisplayingNewMedForm = this.handleDisplayingNewMedForm.bind(this);
+  }
+
+  handleDisplayingNewMedForm(event){
+    this.setState({formVisibleOnPage: true});
+  }
+
+
+  render(){
+    return(
+      <div>
+        <h5>Click the button to begin tracking a medication.</h5>
+        <Button onClick={this.handleDisplayingNewMedForm}>Add Medication</Button>
+        <hr/>
+      </div>
+    );
+  }
 }
 
 export default NewMedControl;
