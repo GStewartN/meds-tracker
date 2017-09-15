a6a1eabf74be55727ff3f49d13af127a1e4d3357
Nathan Stewart <stewart.nathang@gmail.com>
2017-09-15 11:42:22 -0700

    add medlist component with ph medication

--- src/components/Creator.jsx
+++ src/components/Creator.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import MedsList from "./MedsList";
 
-function Creator(){
+function Creator(props){
   return (
     <div>
       <h5>Click the button to begin tracking a medication.</h5>
+      <Button>Add Medication</Button>
+      <hr/>
+      <MedsList/>
     </div>
   );
 }
---
+++ src/components/MedsList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MedsList(props){
+  return (
+    <div>
+      <h5>Medication:</h5>
+      <p><em>Aspirin</em></p>
+    </div>
+  );
+}
+
+export default MedsList;
